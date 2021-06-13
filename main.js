b2388a0eaaee59613ba6d81b839e9d777964aea4
Iuterian99 <Abduhalim.orziqulov@bk.ru>
2021-06-13 12:29:03 +0500

    add half first page

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const elHumbergerBtn = document.querySelector('.hamburger-btn')
-const elHeader = document.querySelector('.header')
+const elHeader = document.querySelector('.site-header')
 
 elHumbergerBtn.addEventListener('click', function () {
 	elHeader.classList.toggle('header--adaptive')
